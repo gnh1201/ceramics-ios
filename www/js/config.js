41e1300a5fcff2b5142584b695b1281ac1334075
user146094 <user146094@AS106.local>
2018-10-29 13:46:56 +0800

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'https://ceramics.databean.co.kr/api/')
+.constant('WORDPRESS_PUSH_URL', 'https://ceramics.databean.co.kr/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+
+;
